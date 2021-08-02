56b56d03f771985d3672d22358eb25a96b832051
diegcarlos <53383018+diegcarlos@users.noreply.github.com>
2021-08-02 17:21:19 -0400

    Build

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import * as Express from 'express'
+import Express from 'express'
 import toKenGn from './router/apiGN/tokenGN'
 import routerCobPix from './router/apiGN/cobGnPix'
 import routerGeQrGn from './router/apiGN/cobGeQrPix'
@@ -8,10 +8,12 @@ const app = Express()
 
 app.use(Express.json())
 
+/** Api Gerencia Net */
 app.use(toKenGn)
 app.use(routerCobPix)
 app.use(routerGeQrGn)
 app.use(routerConCobGn)
+/**  */
 
 app.listen(3333, () => {
   console.log('running')
